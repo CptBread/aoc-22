1df3636117f3e308c0e7a914c8429d91c9d73a75
CptBread <fredrik.astrom89@gmail.com>
2022-12-07 21:07:27 +0100

    Simpliefied day 7 a bit. No need for each directory to actually have a list of each file directly inside it.

--- src/day7.rs
+++ src/day7.rs
@@ -5,43 +5,9 @@ use aoc_util::parse_t::*;
 
 #[derive(Debug)]
 enum FType {
-	Dir(HashSet<PathBuf>),
+	Dir,
 	File(u64),
 }
-
-impl FType {
-	fn add_path(&mut self, path: PathBuf) {
-		if let FType::Dir(paths) = self {
-			paths.insert(path);
-		}
-	}
-
-	fn unwrap_dir(&self) -> &HashSet<PathBuf> {
-		if let FType::Dir(paths) = self {
-			paths
-		}
-		else {
-			panic!()
-		}
-	}
-}
-
-fn calc_sizes(cur_path: &PathBuf, cur_dir: &HashSet<PathBuf>, sizes: &mut HashMap<PathBuf, u64>, fss: &HashMap::<PathBuf, FType>) -> u64 {
-	let mut res = 0;
-	for p in cur_dir {
-		match fss.get(p).unwrap() {
-			FType::File(s) => {
-				res += s;
-			},
-			FType::Dir(ref paths) => {
-				res += calc_sizes(&p, &paths, sizes, fss);
-			}
-		}
-	}
-	sizes.insert(cur_path.clone(), res);
-	res
-}
-
 
 #[allow(dead_code)]
 pub fn solve()
@@ -50,13 +16,9 @@ pub fn solve()
 
 	let mut fss = HashMap::<PathBuf, FType>::new();
 	let mut dir = PathBuf::from("/");
-	fss.insert(dir.clone(), FType::Dir(HashSet::new()));
+	fss.insert(dir.clone(), FType::Dir);
 
 	let root = PathBuf::from("/");
-
-	// println!("{:?}", dir);
-
-	// let mut in_ls = false;
 	for l in data.lines() {
 		if let Some(cd) = l.strip_prefix("$ cd ") {
 			if cd == ".." {
@@ -66,30 +28,35 @@ pub fn solve()
 				dir = root.clone();
 			}
 			else {
-				let add_rotdir = dir == root;
 				let new_dir = dir.join(cd);
-				fss.entry(dir.clone()).or_insert(FType::Dir(HashSet::new()));
-				fss.get_mut(&dir).unwrap().add_path(new_dir.clone());
+				fss.entry(dir.clone()).or_insert(FType::Dir);
 				dir = new_dir;
 			}
 		}
 		else if !l.starts_with("$") {
 			if let Some(d) = l.strip_prefix("dir ") {
-				fss.entry(dir.join(d)).or_insert(FType::Dir(HashSet::new()));
-				fss.get_mut(&dir).unwrap().add_path(dir.join(d));
+				fss.entry(dir.join(d)).or_insert(FType::Dir);
 			}
 			else {
 				let (size, name) = parse_t!(l, u64, " ", PassStr, "").unwrap();
 				let path = dir.join(name);
 				fss.entry(path.clone()).or_insert(FType::File(size));
-				fss.get_mut(&dir).unwrap().add_path(path);
 			}
 		}
 	}
 
 	let mut sizes = HashMap::new();
-	let root_dir = fss.get(&root).unwrap().unwrap_dir();
-	let tot_size = calc_sizes(&root, root_dir, &mut sizes, &fss);
+	let mut tot_size = 0;
+
+	for (p, f) in fss.iter() {
+		if let FType::File(size) = f {
+			tot_size += size;
+			for p in p.ancestors().skip(1) {
+				*sizes.entry(p).or_default() += size;
+			}
+		}
+	}
+
 	let to_free = 30000000 - (70000000 - tot_size);
 
 	let mut part1 = 0;
@@ -102,5 +69,6 @@ pub fn solve()
 			part2 = *s;
 		}
 	}
+	// 1517599 2481982
 	println!("{:?} {}", part1, part2);
 }
