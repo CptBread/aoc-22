4c5d9267d5efb5f88a60519d24f7d819bfac9e4b
CptBread <fredrik.astrom89@gmail.com>
2023-01-18 21:34:35 +0100

    Slow day 19 search

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use aoc_util::parse_t::*;
 
 #[repr(usize)]
@@ -8,6 +10,17 @@ enum Resource {
 	Obsidian,
 	Geode,
 	MAX,
+}
+
+impl Resource {
+	pub fn iter() -> impl Iterator<Item = Resource> {
+		[
+			Resource::Ore,
+			Resource::Clay,
+			Resource::Obsidian,
+			Resource::Geode,
+		].iter().copied()
+	}
 }
 
 type RCount = u32;
@@ -21,43 +34,76 @@ type RCount = u32;
 // 	// geode: [RCount; 2],
 // }
 
-type Bp = [Vec<(Resource, RCount)>; Resource::MAX as usize];
-type State = [(RCount, RCount);4];
+struct BotBp {
+	res: Resource,
+	cost: Vec<(Resource, RCount)>,
+}
 
-fn tick(s: &mut State) {
-	for (c, g) in s {
-		*c += *g;
+type Bp = [BotBp; Resource::MAX as usize];
+
+#[derive(Debug, Clone, Copy)]
+struct State([(RCount, RCount);4]);
+
+impl State {
+	fn tick(&mut self) {
+		for (c, g) in &mut self.0 {
+			*c += *g;
+		}
 	}
 }
 
+
 fn buy_bot(r: Resource, s: &State, bp: &Bp) -> Option<State> {
 	let mut res = s.clone();
-	for (cr, c) in bp[r as usize].iter().cloned() {
-		let d = &mut res[cr as usize];
+	for (cr, c) in bp[r as usize].cost.iter().cloned() {
+		let d = &mut res.0[cr as usize];
 		d.0 = d.0.checked_sub(c)?;
 	}
-	res[r as usize].1 += 1;
+	res.tick();
+	res.0[r as usize].1 += 1;
 	Some(res)
 }
 
 pub fn solve() {
 	let data = std::fs::read_to_string("data/day19.txt").unwrap();
-	let mut bp = Vec::new();
+	let mut bps = Vec::new();
 	for l in data.lines() {
 		// println!("{:?}", parse_t!(l, "Blueprint ", usize, ": Each ore robot costs ", RCount, " ore. Each clay robot costs ", RCount, " ore. Each obsidian robot costs ", RCount, " ore and ", RCount, " clay. Each geode robot costs ", RCount, " ore and ", RCount, " obsidian."));
 		let (_, ooc, coc, boc, bcc, goc, gbc) = parse_t!(l, "Blueprint ", usize, ": Each ore robot costs ", RCount, " ore. Each clay robot costs ", RCount, " ore. Each obsidian robot costs ", RCount, " ore and ", RCount, " clay. Each geode robot costs ", RCount, " ore and ", RCount, " obsidian.").unwrap();
-		bp.push([
-			vec![(Resource::Ore, ooc)],
-			vec![(Resource::Ore, coc)],
-			vec![(Resource::Ore, boc), (Resource::Clay, bcc)],
-			vec![(Resource::Ore, goc), (Resource::Obsidian, gbc)],
+		bps.push([
+			BotBp{ res: Resource::Ore, cost: vec![(Resource::Ore, ooc)]},
+			BotBp{ res: Resource::Clay, cost: vec![(Resource::Ore, coc)]},
+			BotBp{ res: Resource::Obsidian, cost: vec![(Resource::Ore, boc), (Resource::Clay, bcc)]},
+			BotBp{ res: Resource::Geode, cost: vec![(Resource::Ore, goc), (Resource::Obsidian, gbc)]},
 		]);
 	}
-	let mut start: State = [(0,1), (0, 0), (0, 0), (0, 0)];
-	tick(&mut start);
-	tick(&mut start);
-	tick(&mut start);
-	tick(&mut start);
-	dbg!(buy_bot(Resource::Ore, &start, &bp[0]));
-	dbg!(start);
+	let bp: &Bp = &bps[0];
+	// let mut start = State([(0,1), (0, 0), (0, 0), (0, 0)]);
+	dbg!(start_search(24, bp));
+}
+
+fn start_search(mut left: u32, bp: &Bp) -> RCount {
+	let mut state = State([(0,1), (0, 0), (0, 0), (0, 0)]);
+	let skip = bp.iter().fold(99, |acc, b| acc.min(b.cost[0].1));
+	left -= skip;
+	for _ in 0..skip {
+		state.tick();
+	}
+	search(left, state, bp)
+}
+
+fn search(left: u32, mut state: State, bp: &Bp) -> RCount {
+	if left <= 0 {
+		// dbg!(state);
+		return state.0[Resource::Geode as usize].0;
+	}
+	let mut best = 0;
+	for r in Resource::iter() {
+		if let Some(res) = buy_bot(r, &mut state, bp) {
+			best = best.max(search(left - 1, res, bp));
+		}
+	}
+	state.tick();
+	best = best.max(search(left - 1, state, bp));
+	best
 }
